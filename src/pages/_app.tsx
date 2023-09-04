70ae54daabf17bb3b0ede16f080b291bd6bdb7c2
hitee8239 <laby8239@gmail.com>
2023-09-04 00:42:23 +0000

    ad_myportfolio

---
+++ src/pages/_app.tsx
@@ -0,0 +1,61 @@
+import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+
+/**
+ * @description SEO를 위해 본인의 정보로 수정해주세요.
+ */
+const DEFAULT_SEO = {
+  title: "김인철 | junior Dev",
+  description: "안녕하세요, 주니어 개발자 김인철입니다.",
+  canonical: "https://www.naver.com/",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://www.naver.com/",
+    title: "김인철 | junior Dev",
+    site_name: "김인철 | junior Dev",
+    images: [
+      {
+        url: "/share.png",
+        width: 285,
+        height: 167,
+        alt: "김인철 | junior Dev",
+      },
+    ],
+  },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: "application-name",
+      content: "김인철 | junior Dev",
+    },
+    {
+      name: "msapplication-tooltip",
+      content: "김인철 | junior Dev",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+  ],
+};
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default App;
